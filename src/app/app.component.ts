540edbf2fe1ba61b5107350049e52e9b39c09d78
liviasab <liviasabino92@gmail.com>
2025-03-28 20:44:38 -0300

    feat: creating menu title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, 
+  imports: [RouterOutlet, MenuTitleComponent], 
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] 
 })
 export class AppComponent {
   title = 'angular-client';
